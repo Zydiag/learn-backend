5b08e5846a88ed64ed1c21aa7fadc5ff6e23f52e
Sahil <dshiv867@gmail.com>
2024-01-24 14:34:09 +0530

    added watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import { asyncHandler } from '../utils/asyncHandler.js'
 import { apiError } from '../utils/apiError.js'
 import { User } from '../models/user.model.js'
@@ -357,6 +358,54 @@ const getUserProfile = asyncHandler(async (req, res) => {
   console.log(channel) // note
 })
 
+const getUserWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: 'videos',
+        localField: 'watchHistory',
+        foreignField: '_id',
+        as: 'watchHistory',
+        pipeline: [
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'owner',
+              foreignField: '_id',
+              as: 'owner',
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: '$owner',
+              },
+            },
+          },
+        ],
+      },
+    },
+  ])
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, user[0]?.watchHistory, 'fetched watch history'))
+})
+
 export {
   registerUser,
   loginUser,
@@ -368,4 +417,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserProfile,
+  getUserWatchHistory,
 }
